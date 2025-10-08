a113d7122282e63fb7b545522cc9aca5a0579f4d
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-10-08 13:22:17 +0800

    refactor(web): 调整结果包装器配置属性路径
    - 修改 @ConditionalOnProperty 注解中的属性前缀和名称- 将 only.web.enable-result-wrapper 迁移为 only.web.result-wrapper.enable

--- engine-web/src/main/kotlin/com/only/engine/web/advice/StringResponseAdvice.kt
+++ engine-web/src/main/kotlin/com/only/engine/web/advice/StringResponseAdvice.kt
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
 @Order(5)
 @AutoConfiguration
 @RestControllerAdvice(basePackages = ["edu.only"])
-@ConditionalOnProperty(prefix = "only.web", name = ["enable-result-wrapper"], havingValue = "true")
+@ConditionalOnProperty(prefix = "only.web.result-wrapper", name = ["enable"], havingValue = "true")
 class StringResponseAdvice : ResponseBodyAdvice<Any>, WebInitPrinter {
 
     companion object {
